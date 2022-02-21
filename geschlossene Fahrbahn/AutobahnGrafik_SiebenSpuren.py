52e2013f7baec248a862343ac130f47083269e09
Erosace <77454396+Erosace@users.noreply.github.com>
2022-02-21 16:51:05 +0100

    Add files via upload

---
+++ geschlossene Fahrbahn/AutobahnGrafik_SiebenSpuren.py
@@ -0,0 +1,189 @@
+from graphics import * #graphics.py ist eine Zeichenbibliothek
+
+
+class Graphic:
+    _window = None
+
+
+    rechtecke = []
+    upDown = None
+    leftRight = None
+    anzeigetafeln = None
+    abschnitte = None
+
+    anzeigesystem = ["" for x in range(14)]#die ersten 7 sind die Höchstgeschwindigkeit, die letzten 7 die Anzahl der Autos
+
+    #eine Zeile in der Legende ist ca 17 hoch
+    legende = [Rectangle(Point(35,15), Point(300,150)), Text(Point(70,25),"Legende"),Text(Point(46,45),"X"), Text(Point(57,45),"Y"),
+               Text(Point(165,60),"X = erlaubte Höchstgeschwindigkeit"), Text(Point(112,75), "Y = Anzahl der Autos"),
+               Text(Point(70,89), "7 = vmax"), Text(Point(88,105),"p = Pausieren"), Text(Point(77,120), "s = Starten"),
+               Text(Point(70, 135), "Farbe(v):"),
+               Rectangle(Point(110,130), Point(120,140)), Rectangle(Point(150,130), Point(160,140)), Rectangle(Point(190,130), Point(200,140)),
+               Rectangle(Point(230,130), Point(240,140)),
+               Text(Point(135, 135), "0,1"), Text(Point(175, 135), "2,3"), Text(Point(215, 135), "4,5"), Text(Point(255, 135), "6,7")]
+    legende[1].setTextColor("blue")
+    legende[2].setTextColor("red")
+    legende[3].setTextColor("red")
+
+    legende[10].setFill("red")
+    legende[11].setFill("brown")
+    legende[12].setFill("DodgerBlue4")
+    legende[13].setFill("turquoise3")
+
+
+    def __init__(self, windowWidth, windowHeight, leftRight, upDown):
+        self.upDown = upDown
+        self.leftRight = leftRight
+        self._window = GraphWin("Autobahnsimulation", windowWidth, windowHeight, autoflush=False)
+
+        # Ecken Breite berechnen:
+        # 2 * eckeY + upDown * zellenHöhe = windowHeight
+        # Aber eckeY muss auch genauso groß sein wie zellenHöhe, damit es gut aussieht
+        # also 2 * zellenHöhe + upDown * zellenHöhe = windowHeight
+        # zellenHöhe = windowHeight / (upDown + 2)
+        # zellenBreite = windowWidth / (leftRight + 2)
+
+        self.zellenHöhe = windowHeight / (leftRight + 2)
+        self.zellenBreite = windowWidth / (upDown + 2)
+        zellenHöhe = windowHeight / (leftRight + 2)#erspart Arbeit, ansonsten muss überall self. for jedem zellenHöhe/-Breite
+        zellenBreite = windowWidth / (upDown + 2)
+
+        # Autobahn in E-Schlangenform mit 200 Zellen, 7 Abschnitte, Fensterbreite 1400, Fensterhöhe 1000
+        #erste obere Reihe
+        for i in range(200):
+            self.rechtecke.append(Rectangle(Point(zellenBreite + i * zellenBreite, 0), Point(zellenBreite + (i + 1) * zellenBreite, zellenHöhe)))
+        #zweite Reihe (rechts,Knick)
+        for i in range(40):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite, zellenHöhe + i * zellenHöhe), Point(windowWidth, zellenHöhe + (i+1) * zellenHöhe)))
+        #zweite Reihe (Horizontale)
+        for i in range(40,190):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite*(i-38), zellenHöhe + 39 * zellenHöhe), Point(windowWidth - zellenBreite*(i-39), zellenHöhe + (40) * zellenHöhe)))
+        #zweite Reihe links
+        for i in range(190,200):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite*(190-38), zellenHöhe + (39+i-190) * zellenHöhe), Point(windowWidth - zellenBreite*(190-39), zellenHöhe + (40+i-190) * zellenHöhe)))
+        #dritte Reihe links
+        for i in range(0,30):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite*(190-38), zellenHöhe + (39+i+10) * zellenHöhe), Point(windowWidth - zellenBreite*(190-39), zellenHöhe + (40+i+10) * zellenHöhe)))
+        #dritte Reihe horizontal
+        for i in range(30,180):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite*(190-38-(i-29)), zellenHöhe + (39+29+10) * zellenHöhe), Point(windowWidth - zellenBreite*(190-39-(i-29)), zellenHöhe + (40+29+10) * zellenHöhe)))
+        #dritte Reihe rechts
+        for i in range(180,200):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite, zellenHöhe + (39+29+10+i-180) * zellenHöhe), Point(windowWidth, zellenHöhe + (40+29+10+i-180) * zellenHöhe)))
+        #vierte Reihe rechts
+        for i in range(0,20):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite, zellenHöhe + (39+30+10+20+i) * zellenHöhe), Point(windowWidth, zellenHöhe + (40+30+10+20+i) * zellenHöhe)))
+        #vierte Reihe horizontal
+        for i in range(20,170):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite*(i-18), zellenHöhe + (39+29+10+20+20) * zellenHöhe), Point(windowWidth - zellenBreite*(i-19),  zellenHöhe + (40+29+10+20+20) * zellenHöhe)))
+        #vierte Reihe links
+        for i in range(170,200):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite*(190-38), zellenHöhe + (39+29+10+20+20+i-170) * zellenHöhe), Point(windowWidth - zellenBreite*(190-39),  zellenHöhe + (40+29+10+20+20+i-170) * zellenHöhe)))
+        #fünfte Reihe links
+        for i in range(0,10):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite*(190-38), zellenHöhe + (39+29+10+20+20+30+i) * zellenHöhe), Point(windowWidth - zellenBreite*(190-39), zellenHöhe + (40+29+10+20+20+30+i) * zellenHöhe)))
+        #fünfte Reihe horizontal
+        for i in range(10,160):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite*(190-38-(i-9)), zellenHöhe + (39+29+10+20+20+30+9) * zellenHöhe), Point(windowWidth - zellenBreite*(190-38-(i-10)), zellenHöhe + (40+29+10+20+20+30+9) * zellenHöhe)))
+        #fünfte Reihe rechts
+        for i in range(160,200):
+            self.rechtecke.append(Rectangle(Point(windowWidth - zellenBreite, zellenHöhe + (39+29+10+20+20+30+9+i-160) * zellenHöhe), Point(windowWidth, zellenHöhe + (40+29+10+20+20+30+9+i-160) * zellenHöhe)))
+        #sechste Reihe horizontal
+        for i in reversed(range(200)):
+            self.rechtecke.append(Rectangle(Point(zellenBreite+i*zellenBreite, windowHeight - zellenHöhe), Point(zellenBreite+(i+1)*zellenBreite, windowHeight)))
+        #siebte Reihe links
+        for i in reversed(range(leftRight)):
+            self.rechtecke.append(Rectangle(Point(0, zellenHöhe + i * zellenHöhe), Point(zellenBreite, zellenHöhe + (i+1) * zellenHöhe)))
+
+        self.anzeigetafeln = [Rectangle(Point(98 * zellenBreite, 2 * zellenHöhe), Point(106 * zellenBreite, 6 * zellenHöhe)),
+                              Rectangle(Point(126 * zellenBreite, 47 * zellenHöhe), Point(134 * zellenBreite, 51 * zellenHöhe)),
+                              Rectangle(Point(126 * zellenBreite, 68 * zellenHöhe), Point(134 * zellenBreite, 72 * zellenHöhe)),
+                              Rectangle(Point(126 * zellenBreite, 124 * zellenHöhe), Point(134 * zellenBreite, 128 * zellenHöhe)),
+                              Rectangle(Point(126 * zellenBreite, 152 * zellenHöhe), Point(134 * zellenBreite, 156 * zellenHöhe)),
+                              Rectangle(Point(98 * zellenBreite, 194 * zellenHöhe), Point(106 * zellenBreite, 198 * zellenHöhe)),
+                              Rectangle(Point(4 * zellenBreite, 96 * zellenHöhe), Point(12 * zellenBreite, 100 * zellenHöhe)),
+                              Line(Point(102 * zellenBreite, 2 * zellenHöhe), Point(102 * zellenBreite, 6 * zellenHöhe)),
+                              Line(Point(130 * zellenBreite, 47 * zellenHöhe), Point(130 * zellenBreite, 51 * zellenHöhe)),
+                              Line(Point(130 * zellenBreite, 68 * zellenHöhe), Point(130 * zellenBreite, 72 * zellenHöhe)),
+                              Line(Point(130 * zellenBreite, 124 * zellenHöhe), Point(130 * zellenBreite, 128 * zellenHöhe)),
+                              Line(Point(130 * zellenBreite, 152 * zellenHöhe), Point(130 * zellenBreite, 156 * zellenHöhe)),
+                              Line(Point(102 * zellenBreite, 194 * zellenHöhe), Point(102 * zellenBreite, 198 * zellenHöhe)),
+                              Line(Point(8 * zellenBreite, 96 * zellenHöhe), Point(8 * zellenBreite, 100 * zellenHöhe))]
+
+        self.abschnitte = [Line(Point(0,0), Point(zellenBreite * 4, zellenHöhe * 4)), Line(Point((self.upDown + 2) * zellenBreite - 4 * self.zellenBreite, 4 * self.zellenHöhe), Point((self.upDown + 2) * zellenBreite, 0)),
+                      Line(Point(48 * self.zellenBreite, 50 * zellenHöhe), Point(54 * self.zellenBreite, 50 * zellenHöhe)),
+                      Line(Point((self.upDown + 2) * zellenBreite - 4 * self.zellenBreite, 99.5 * zellenHöhe), Point((self.upDown + 2) * zellenBreite, 99.5 * zellenHöhe)),
+                      Line(Point(48 * self.zellenBreite, 149 * zellenHöhe), Point(54 * self.zellenBreite, 149 * zellenHöhe)),
+                      Line(Point((self.upDown + 2) * zellenBreite - 4 * self.zellenBreite, (self.upDown + 2) * zellenHöhe - 4 * self.zellenHöhe), Point((self.upDown + 2) * zellenBreite, (self.upDown + 2) * zellenHöhe)),
+                      Line(Point(0, (self.upDown + 2) * zellenHöhe), Point(4 * self.zellenBreite, (self.upDown + 2) * zellenHöhe - 4 * self.zellenHöhe))]
+
+
+        #Initialisieren
+        for x in self.rechtecke:
+            x.setFill("white")
+            x.setOutline("gray")
+            x.draw(self._window)
+
+        for x in self.legende:
+            x.draw(self._window)
+
+        for x in self.anzeigetafeln:
+            x.draw(self._window)
+
+        for x in self.abschnitte:
+            x.draw(self._window)
+
+
+    def showCars(self, Autobahn,  geschwindigkeitsbegrenzung):
+        for i in range(len(Autobahn)):
+            if Autobahn[i][0] == 1:
+                self.rechtecke[i].setFill(Farbe(Autobahn, i))
+
+        self.anzeigesystem[0] = (Text(Point(100*self.zellenBreite, 4*self.zellenHöhe), "{}".format(geschwindigkeitsbegrenzung[0])))
+        self.anzeigesystem[1] = (Text(Point(128*self.zellenBreite, 49*self.zellenHöhe), "{}".format(geschwindigkeitsbegrenzung[1])))
+        self.anzeigesystem[2] = (Text(Point(128*self.zellenBreite, 70*self.zellenHöhe), "{}".format(geschwindigkeitsbegrenzung[2])))
+        self.anzeigesystem[3] = (Text(Point(128*self.zellenBreite, 126*self.zellenHöhe), "{}".format(geschwindigkeitsbegrenzung[3])))
+        self.anzeigesystem[4] = (Text(Point(128*self.zellenBreite, 154*self.zellenHöhe), "{}".format(geschwindigkeitsbegrenzung[4])))
+        self.anzeigesystem[5] = (Text(Point(100*self.zellenBreite, 196*self.zellenHöhe), "{}".format(geschwindigkeitsbegrenzung[5])))
+        self.anzeigesystem[6] = (Text(Point(6*self.zellenBreite, 98*self.zellenHöhe), "{}".format(geschwindigkeitsbegrenzung[6])))
+
+        self.anzeigesystem[7] = (Text(Point(104*self.zellenBreite, 4*self.zellenHöhe), "{}".format(AutosZählen(Autobahn, 0,200))))
+        self.anzeigesystem[8] = (Text(Point(132*self.zellenBreite, 49*self.zellenHöhe), "{}".format(AutosZählen(Autobahn,200,400))))
+        self.anzeigesystem[9] = (Text(Point(132*self.zellenBreite, 70*self.zellenHöhe), "{}".format(AutosZählen(Autobahn,400,600))))
+        self.anzeigesystem[10] = (Text(Point(132*self.zellenBreite, 126*self.zellenHöhe), "{}".format(AutosZählen(Autobahn,600,800))))
+        self.anzeigesystem[11] = (Text(Point(132*self.zellenBreite, 154*self.zellenHöhe), "{}".format(AutosZählen(Autobahn,800,1000))))
+        self.anzeigesystem[12] = (Text(Point(104*self.zellenBreite, 196*self.zellenHöhe), "{}".format(AutosZählen(Autobahn,1000,1200))))
+        self.anzeigesystem[13] = (Text(Point(10*self.zellenBreite, 98*self.zellenHöhe), "{}".format(AutosZählen(Autobahn,1200,1400))))
+
+        for i in range(len(geschwindigkeitsbegrenzung*2)):
+            self.anzeigesystem[i].setSize(int(2.5*self.zellenHöhe)+1)
+            self.anzeigesystem[i].draw(self._window)
+
+    #färbt die Zellen weiß, welche auf der neuen Bahn nicht mehr belegt sind
+    def setWhite(self, alteAutobahn, neueAutobahn):
+        for i in range(len(alteAutobahn)):
+            if alteAutobahn[i][0] != neueAutobahn[i][0]:
+                self.rechtecke[i].setFill("white")
+                self.rechtecke[i].setOutline("gray")
+        for x in self.anzeigesystem:
+            x.setTextColor("white")
+
+
+def AutosZählen(Autobahn, ersteZelle, letzteZelle):
+    a = 0
+    for i in range(ersteZelle, letzteZelle):
+        if Autobahn[i][0] == 1:
+            a += 1
+    return (a)
+
+def Farbe(Autobahn, Zelle):
+    a = Autobahn[Zelle][1]
+    if a == 0 or a == 1:
+        return("red")
+    if a == 2 or a == 3:
+        return("brown")
+    if a == 4 or a == 5:
+        return("DodgerBlue4")
+    if a == 6 or a == 7:
+        return("turquoise3")
+
---
+++ geschlossene Fahrbahn/einspurigeAutobahn_SiebenSpuren.py
@@ -0,0 +1,133 @@
+from AutobahnGrafik_SiebenSpuren import *
+import random
+import time
+
+
+def neueSpur(Fahrbahnlänge, Geschwindigkeitsbegrenzung):
+    a = [[0, 3, 0] for x in range(Fahrbahnlänge)] #3 damit die Autos nicht aus dem Stand aus anfahren
+    a = GeschwindigkeitsbegrenzungEinführen(a, Geschwindigkeitsbegrenzung)
+    return (a)
+
+def neueFahrzeuge(Fahrbahn, AnzahlFahrzeuge):
+    b = int(len(Fahrbahn) / AnzahlFahrzeuge)
+    for i in range(AnzahlFahrzeuge):
+        Fahrbahn[b * i][0] = 1
+    return (Fahrbahn)
+
+def AbstandzumVordermann(Autobahn, Index):
+    a = len(Autobahn)
+    counter = 0
+    for i in range(1, a + 1):
+        if Autobahn[(Index + i) % a][0] == 1:
+            counter += 1
+            return (counter)
+        else:
+            counter += 1
+
+def Fahrbahnkopieren(Autobahn):
+    return [Autobahn[x] for x in range(len(Autobahn))]
+
+
+def nächsteZeiteinheit(Autobahn, p, vmax, Geschwindigkeitsbegrenzung):
+    neueS = neueSpur(len(Autobahn), Geschwindigkeitsbegrenzung)
+    for i in range(len(Autobahn)):
+        if Autobahn[i][0] == 1:
+            b = AbstandzumVordermann(Autobahn, i) - 1
+            c = random.uniform(0, 1)
+            d = p[Autobahn[i][1]]
+            if Autobahn[i][1] != 0 and c <= d:  # Trödeln
+                Autobahn[i][1] = Autobahn[i][1] - 1
+            elif c > d and b > Autobahn[i][1] and Autobahn[i][1] < vmax and Autobahn[i][1] < Autobahn[i][
+                2]:  # Schnellestmögliches Fahren
+                Autobahn[i][1] = Autobahn[i][1] + 1
+
+            if b < Autobahn[i][1]:  # Sicherheitsabstand
+                Autobahn[i][1] = b
+            if (i + Autobahn[i][1]) >= len(Autobahn):
+                neueS[(i + Autobahn[i][1]) % len(Autobahn)][0] = 1
+                neueS[(i + Autobahn[i][1]) % len(Autobahn)][1] = Autobahn[i][1]
+                continue
+            else:
+                neueS[i + Autobahn[i][1]] = Autobahn[i]
+    return (neueS)
+
+def AutosZählen(Autobahn, ersteZelle, letzteZelle):
+    a = 0
+    for i in range(ersteZelle, letzteZelle):
+        if Autobahn[i][0] == 1:
+            a += 1
+    return (a)
+
+def Höchstgeschwindigkeit(Dichte):
+    # bestimmt die Höchstgeschwindigkeit eines Abschnittes anhand der Verkehrsdichte
+    if Dichte == 0:
+        return 100
+    else:
+        return ((int((1 / Dichte) - 2 )))
+
+def neueGeschwindigkeitsbegrenzung(Autobahn):
+    #berechnet die Dichte Autos/Zelle
+    a = AutosZählen(Autobahn, 0, int(len(Autobahn) / 7)) / (len(Autobahn) / 7)
+    b = AutosZählen(Autobahn, int(len(Autobahn) / 7), int(2 * len(Autobahn) / 7)) / (len(Autobahn) / 7)
+    c = AutosZählen(Autobahn, int(2 * len(Autobahn) / 7), int(3 * len(Autobahn) / 7)) / (len(Autobahn) / 7)
+    d = AutosZählen(Autobahn, int(3 * len(Autobahn) / 7), int(4 * len(Autobahn) / 7)) / (len(Autobahn) / 7)
+    e = AutosZählen(Autobahn, int(4 * len(Autobahn) / 7), int(5 * len(Autobahn) / 7)) / (len(Autobahn) / 7)
+    f = AutosZählen(Autobahn, int(5 * len(Autobahn) / 7), int(6 * len(Autobahn) / 7)) / (len(Autobahn) / 7)
+    g = AutosZählen(Autobahn, int(6 * len(Autobahn) / 7), int(7 * len(Autobahn) / 7)) / (len(Autobahn) / 7)
+    return ([Höchstgeschwindigkeit(b), Höchstgeschwindigkeit(c), Höchstgeschwindigkeit(d), Höchstgeschwindigkeit(e), Höchstgeschwindigkeit(f), Höchstgeschwindigkeit(g), Höchstgeschwindigkeit(a)])
+
+def GeschwindigkeitsbegrenzungEinführen(a, Geschwindigkeitsbegrenzung):
+    for i in range(len(a)):
+        if i < len(a) / 7:
+            a[i][2] = Geschwindigkeitsbegrenzung[0]
+        elif i < 2*len(a) / 7:
+            a[i][2] = Geschwindigkeitsbegrenzung[1]
+        elif i < 3 * len(a) / 7:
+            a[i][2] = Geschwindigkeitsbegrenzung[2]
+        elif i < 4*len(a) / 7:
+            a[i][2] = Geschwindigkeitsbegrenzung[3]
+        elif i < 5*len(a) / 7:
+            a[i][2] = Geschwindigkeitsbegrenzung[4]
+        elif i < 6*len(a) / 7:
+            a[i][2] = Geschwindigkeitsbegrenzung[5]
+        elif i < len(a):
+            a[i][2] = Geschwindigkeitsbegrenzung[6]
+    return (a)
+
+
+def Autobahn():
+    Geschwindigkeitsbegrenzung = [10, 10, 10, 10, 10, 10, 10]
+    # "Wie lange soll die Autobahn sein"
+    a = 200  # 1/7 der Zellen
+    # "Was ist die Höchstgeschwindigkeit der Fahrzeuge?"
+    vmax = 7
+    # "Was ist die Wahrscheinlichkeit, dass ein Auto trödelt?"
+    p = [0.22, 0.2, 0.16, 0.13, 0.11, 0.08, 0.06, 0.05]
+    Autobahn = neueSpur(a * 7, Geschwindigkeitsbegrenzung)
+    #Anzahl der Fahrzeuge
+    Autobahn = neueFahrzeuge(Autobahn, 200)
+
+    window = Graphic(1400, 1000, a, a)
+    window.showCars(Autobahn, Geschwindigkeitsbegrenzung)
+
+    count = 0
+    warten = 0
+    while count < 1000:
+
+        if warten == 1:
+            if window._window.getKey() == "s":#s für starten
+                warten = 0
+        Geschwindigkeitsbegrenzung = neueGeschwindigkeitsbegrenzung(Autobahn)
+        alteAutobahn = Fahrbahnkopieren(Autobahn)
+        Autobahn = GeschwindigkeitsbegrenzungEinführen(Autobahn, Geschwindigkeitsbegrenzung)
+        Autobahn = nächsteZeiteinheit(Autobahn, p, vmax, Geschwindigkeitsbegrenzung)
+        window.setWhite(alteAutobahn, Autobahn)
+        window.showCars(Autobahn, Geschwindigkeitsbegrenzung)
+        if warten == 0:
+            if window._window.checkKey() == "p":
+                warten = 1
+        time.sleep(0.15)
+        count +=1
+
+#Die Simulation pausiert duch Drücken von "p" und startet wieder durch Drücken von "s". Durch wiederholtes Drücken von "p" geht man nur einen Zeitschritt vorwärts
+Autobahn()
